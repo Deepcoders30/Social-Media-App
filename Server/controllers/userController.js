2fba8ecb21b1a757717083f782e38099e7d12305
umair khan <umair.khan.rocko@gmail.com>
2023-02-09 01:18:00 +0530

    Follow/Unfollow Functionality added

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,4 +1,45 @@
+const User=require("../models/Users.js");
+const {success, error}=require("../utils/responseWrapper.js")
 
-const followUser = async (req, res){
-            
+
+
+const followOrUnfollowUser = async (req, res)=>{
+try{
+  const userIdToFollow = req.body;
+  const currentUserId=req._id;
+
+  const userToFollow=await User.findById(userIdToFollow);
+  const curUser=await User.findById(currentUserId);
+  if(!userToFollow){
+        return res.send(error(484, "User to Follow not found"));
+  }
+
+  if(curUser.followings.includes(userIdToFollow)){//Already Followed
+       const followingIndex=curUser.followings.indexOf(userIdToFollow);
+       curUser.followings.splice(followingIndex, 1);    
+
+       const followerIndex = userToFollow.followers.indexOf(curUser);
+       userToFollow.followers.splice(followerIndex, 1);
+
+       await userToFollow.save();
+       await curUser.save();
+
+       return res.send(success(200, "User Unfollowed"));
+  }
+  else{
+    userToFollow.followers.push(currentUserId);
+    curUser.followings.push(userIdToFollow);
+
+    await userToFollow.save();
+    await curUser.save();
+
+       return res.send(success(200, "User Followed"));
+  }
+}catch(e){
+  res.send(error(500, e.message));
 }
+  
+  
+}
+
+module.exports={followOrUnfollowUser};
