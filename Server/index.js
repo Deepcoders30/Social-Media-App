33750ba72631b18e7aea12df607189a44285e201
umair khan <umair.khan.rocko@gmail.com>
2023-02-09 01:16:43 +0530

    UserRouter added

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,7 @@ dotenv.config("./.env");
 const DBconnect=require("./DBconnect.js");
 const authRouter=require("./routers/authRouter.js");
 const postsRouter=require("./routers/postsRouter.js");
+const userRouter=require("./routers/userRouter.js");
 const morgon=require("morgan");
 const cookieParser=require("cookie-parser");
 const cors=require("cors");
@@ -23,6 +24,7 @@ app.use(cors({
 
 app.use("/auth", authRouter);
 app.use("/posts", postsRouter);
+app.use("/user", userRouter);        
 app.get("/", (req, res)=>{
     res.status(200).send("OK from Server");
 });
