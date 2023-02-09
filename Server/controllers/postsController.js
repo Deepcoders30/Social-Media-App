82de69d11a1058fb462a40a4bd9105ac80dfa662
umair khan <umair.khan.rocko@gmail.com>
2023-02-10 04:44:32 +0530

    Update and Delete post feature

--- Server/controllers/postsController.js
+++ Server/controllers/postsController.js
@@ -3,12 +3,6 @@ const Post=require("../models/Posts.js");
 const User=require("../models/Users.js")
 
 
-
-const getAllPostsController=async (req, res)=>{
-    console.log(req._id)
-    res.send(success(200, "These are all the posts"));
-}
-
 const createPostController= async (req, res)=>{
 
     try{
@@ -62,12 +56,69 @@ const likeAndunlikePost = async (req, res)=>{
     res.send(error(500, e.message));
 }
 
+}
+
+const updatePostController = async (req, res)=>{
+
+try{
+    const {postId, caption}=req.body;
+    const curUserId=req._id;
+
+    const post=await Post.findById(postId);
+
+    if(!post){
+        return res.send(error(404, "Post not found"));  
+    }
+
+    if(post.owner.toString() !== curUserId){
+        return res.send(error(403, "Only owners can update their posts"));
+    }
+
+    if(caption){
+        post.caption=caption;
+    }
+
+    await post.asve();
+
+    return res.semd(success(200, post));
+}catch(e){
+    return res.send(error(500, e.message));
+}
+
+
+}
+
+const deletePost = async (req, res)=>{
+  try{
+    const {postId}=request.body;
+    const currentUserId=req._id;
+
+    const post=await Post.findById(postId);
+    const curUser=await User.findById(currentUserId);
+
+    if(!post){
+        return res.send(error(404, "Post not found"));  
+    }
+
+    if(post.owner.toString() !== curUserId){
+        return res.send(error(403, "Only owners can delete their posts"));
+    }
+
+    const index=curUser.posts.indexOf(postId);
+    curUser.posts.splice(index, 1);
+
+    await curUser.save();
+    await post.remove();
+
+    return res.send(success(200, "post Deleted Successfully"));
+  }catch(e){
+    res.send(error(500, e.message));
+  }
+
     
 }
 
 
 
-
-
-module.exports={getAllPostsController, createPostController, likeAndunlikePost};
+module.exports={createPostController, likeAndunlikePost, updatePostController, deletePost};
 
