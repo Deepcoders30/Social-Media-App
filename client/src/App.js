71d98d3e042f0b7ac945dfe3933c7502f2b87d6b
umair khan <umair.khan.rocko@gmail.com>
2023-02-26 22:19:41 +0530

    Routes Added

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,22 @@ import Signup from "./pages/signup/Signup.js";
 import Home from "./pages/home/Home.js";
 import {Routes, Route} from "react-router-dom";
 import RequireUser from "./components/RequireUser.js";
+import Profile from "./components/profile/Profile.js";
+import Feed from "./components/feed/Feed.js";
+import UpdateProfile from "./components/updateProfile/UpdateProfile.js";
 
 function App() {
   return (
     <div className="App">
      <Routes>
       
-     <Route element={<RequireUser />}>
-     <Route path="/home" element={<Home />} />  
-     </Route>
+     {/* <Route element={<RequireUser />}> */}
+         <Route element={<Home />}> 
+            <Route path="/" element={<Feed />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+            <Route path="/updateProfile" element={<UpdateProfile />} />
+        </Route>
+     {/* </Route> */}
 
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
