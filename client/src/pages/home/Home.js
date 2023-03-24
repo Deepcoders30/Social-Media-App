e729f8846afa17f01c1e775848e9b1b4632d7728
umair khan <umair.khan.rocko@gmail.com>
2023-03-25 01:29:11 +0530

    Home component changes

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../../components/navbar/Navbar.js';
 import "./Home.scss";
 import { Outlet } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getMyInfo } from '../../redux/slices/appConfigSlice.js';
 
 
 function Home() {
+  const dispatch=useDispatch();
+  useEffect(()=>{
+    dispatch(getMyInfo())
+}, []);
   return (
     <>
       <Navbar />
