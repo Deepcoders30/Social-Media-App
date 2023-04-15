bd1a2625f234150b2b9ef0a33b24cac311374589
umair khan <umair.khan.rocko@gmail.com>
2023-04-15 23:21:00 +0530

    React-hot-toast added

--- client/src/utils/axiosClient.js
+++ client/src/utils/axiosClient.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { getItem, KEY_ACCESS_TOKEN, removeItem, setItem } from "./localStorageManager";
+import store from "../redux/store";
+import { setLoading, showToast } from "../redux/slices/appConfigSlice";
+import { TOAST_FAILURE } from "../App";
 
 
 export const axiosClient=axios.create({
@@ -13,6 +16,7 @@ axiosClient.interceptors.request.use(
         const accessToken=getItem(KEY_ACCESS_TOKEN);
        
         request.headers['Authorization']=`Bearer ${accessToken}`;
+        store.dispatch(setLoading(true));
 
         return request;
     }
@@ -20,6 +24,7 @@ axiosClient.interceptors.request.use(
 
 axiosClient.interceptors.response.use(
     async (response)=>{
+        store.dispatch(setLoading(false));
         const data=response.data;
         if(data.status==='Ok'){
             return data;
@@ -27,8 +32,12 @@ axiosClient.interceptors.response.use(
         
         const originalRequest=response.config;
         const statusCode=data.statusCode;
-        const error=data.error;
+        const error=data.message;
         
+        store.dispatch(showToast({
+            type: TOAST_FAILURE,
+            message: error
+    }))
         //When refresh token expires, send user to login page
         if(statusCode===401 && originalRequest.url===`${process.env.REACT_APP_SERVER_BASE_URL}/auth/refresh`){
                removeItem(KEY_ACCESS_TOKEN);
@@ -58,9 +67,15 @@ axiosClient.interceptors.response.use(
                removeItem(KEY_ACCESS_TOKEN);
                window.location.replace('/login', '_self');
                return Promise.reject(error);
-            }
-
-            
+            }   
         }
+      return Promise.reject(error);
+    },  async(error) => {
+        store.dispatch(setLoading(false));
+        store.dispatch(showToast({
+            type: TOAST_FAILURE,
+            message: error.message
+        }))
+        return Promise.reject(error);
     }
 );
